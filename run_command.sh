3eb86e2f1d5992e64de2a76283ace6fb6ee13a5f
DuyLe <lengocduy169@gmail.com>
2024-10-03 14:38:40 +0700

    Cleaning and importing to database

--- run_command.sh
+++ run_command.sh
@@ -6,8 +6,8 @@ do
   # Run the command
   actiona --execute script.ascr -E -C -x
  
-  # Generate a random sleep time between 30 and 70 seconds
-  sleep_time=$((30 + RANDOM % 40))
+  # Generate a random sleep time between 30 and 80 seconds
+  sleep_time=$((30 + RANDOM % 50))
  
   # Print the random sleep time (optional, for debugging)
   echo "Sleeping for $sleep_time seconds..."
